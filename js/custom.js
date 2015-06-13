d159982124461dd832a07696440b55b23dc97b9e
ShreyKumar <shreyansh.kumar@mail.utoronto.ca>
2015-06-13 16:27:12 -0400

    finished website 2.0

--- js/custom.js
+++ js/custom.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 
     // Scrolls to the selected menu item on the page
     $(function() {
-            $('a[href*=#]:not([href=#])').click(function() {
+            $('a[href*=#]:not([href=#]), a.port-btn').click(function() {
                 if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
                 var target = $(this.hash);
@@ -29,36 +29,5 @@ $(document).ready(function(){
 
 
         });
-    
-        /*
-    $("#hey").hide(0).delay(500).fadeIn(2000);
-    $("#this").hide(0).delay(700).fadeIn(2000);
-    $("#is").hide(0).delay(1000).fadeIn(2000);
-    $("#shrey").hide(0).delay(1600).fadeIn(2000);
-    
-    $("#developer").hide(0).delay(1800).fadeIn(2000);
-    $("#programmer").hide(0).delay(2200).fadeIn(2000);
-    $("#enthusiast").hide(0).delay(2400).fadeIn(2000);
-    
-    $("#buttons").hide(0).delay(3000).show();
-    */
-    $(window).on('resize', function(){
-        /*
-            if($(window).width() <= 991){
-                $("#right").removeClass('right');
-            } else {
-                $("#right").addClass('right');
-            }
-        });
-        */
-    }); 
-    
-    
-    //sync connections
-    $("div.portfolio-item#identity").scroll(function(){
-      alert('hello');  
     });
-    
-    //css button fix
-    
 });
